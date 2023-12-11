5ff9121c43b6ca11d145dbc968eea493b426cbf4
Fer <ferp@protonmail.ch>
2023-12-11 20:42:55 +0100

    add messages component

--- src/Books/Books.tsx
+++ src/Books/Books.tsx
@@ -5,15 +5,19 @@ import { BooksPresenter } from './BooksPresenter';
 import { BookList } from './BookList/BookList';
 import { AddBooks } from './AddBooks/AddBooks';
 import { LastAddedBook } from './LastAddedBook/LastAddedBook';
+import { MessagesComponent } from '../Core/Messages/MessagesComponent';
 
 const BooksComp = observer(({ booksPresenter }) => {
     return (
         <div>
             <h1>Books</h1>
             <LastAddedBook lastAddedBook={booksPresenter.lastAddedBook} />
+            <br/>
             <AddBooks />
-            <p>{booksPresenter.messagePm}</p>
+            <br/>
             <BookList />
+            <br/>
+            <MessagesComponent />
         </div>
     )
 })
