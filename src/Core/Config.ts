09c89b742373a93f0fe3c29c247fc3ada3daed1e
Fer <ferp@protonmail.ch>
2024-01-15 10:42:28 +0100

    env variable support

--- src/Core/Config.ts
+++ src/Core/Config.ts
@@ -1,10 +1,11 @@
 import { injectable } from "inversify";
+const API_URL = import.meta.env.VITE_DEV_API_URL;
 
 @injectable()
 export class Config {
   apiUrl: string;
   constructor() {
     // this.apiUrl = "https://api.logicroom.co/secure-api/ferp@protonmail.ch";
-    this.apiUrl = "http://localhost:3001"
+    this.apiUrl = API_URL || "http://localhost:3000"
   }
 }
